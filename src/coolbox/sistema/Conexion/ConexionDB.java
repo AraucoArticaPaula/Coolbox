d4403384ae0a0473a3178cc76adf287d8d82e47f
AraucoArticaPaula <u23318980@utp.edu.pe>
2026-06-15 17:55:04 -0500

    V1.0.1

--- src/coolbox/sistema/Conexion/ConexionDB.java
+++ src/coolbox/sistema/Conexion/ConexionDB.java
@@ -6,10 +6,12 @@ import java.sql.SQLException;
 
 public class ConexionDB {
 
-    private static final String URL = 
-        "jdbc:sqlserver://localhost;databaseName=CoolboxDB;integratedSecurity=true;encrypt=false;trustServerCertificate=true;";
+    private static final String URL =
+        "jdbc:sqlserver://localhost;databaseName=CoolboxDB;encrypt=false;trustServerCertificate=true;";
+    private static final String USER = "sa";        // tu usuario SQL Server
+    private static final String PASSWORD = "12345"; // tu contraseña SQL Server
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
